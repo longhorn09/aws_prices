019aafb5100c005e67fe317e19241a277e0d0355
Norman Tang <norman@normstorm.com>
2020-07-30 18:39:30 -0500

    progress using bulk api and sku api

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from datetime import datetime, date,time
 from datetime import timezone       # use python3
+import re                           # for regular expression, to parse out instance size from instanceType attribute
 import calendar                     # need this module for last day of month
 import json                         # need this library to interact with JSON data structures
 import urllib.request               # need this library to open up remote website (ie. controltower)
@@ -62,7 +63,32 @@ class AWSPricing:
         elif pArg1 == "SYD":
             retvalue = 'ap-southeast-2'        
         return retvalue
-
+#######################################################################
+    # for use with ["attributes"]["location"] in SKU JSON
+    #######################################################################
+    def getAWSLocationFromCode(self,pArg1):
+        retvalue = None
+        if pArg1 == 'CMH':  # Columbus, OH
+            retvalue = 'US East (Ohio)'                
+        elif pArg1 == "DUB":    # Dublin, IE
+            retvalue = 'EU (Ireland)'
+        elif pArg1 == "FRA":    # Frankfurt, GR
+            retvalue = 'EU (Frankfurt)'
+        elif pArg1 == "GRU":    # Sao Paulo, BR
+            retvalue = 'South America (Sao Paulo)'    
+        elif pArg1 == "IAD":
+            retvalue = "US East (N. Virginia)"
+        elif pArg1 == "LHR":
+            retvalue = 'EU (London)'    #'eu-west-2'
+        elif pArg1 == "NRT":    
+            retvalue = 'Asia Pacific (Tokyo)'   #'ap-northeast-1'
+        elif pArg1 == "PDX":
+            retvalue = 'US West (Oregon)'   #'us-west-2'
+        elif pArg1 == "SIN":
+            retvalue = 'Asia Pacific (Singapore)'   #'ap-southeast-1'
+        elif pArg1 == "SYD":
+            retvalue = 'Asia Pacific (Sydney)'  #'ap-southeast-2'        
+        return retvalue
     #######################################################################
     # URL lookup for region SP version  Url
     #######################################################################
@@ -98,6 +124,38 @@ class AWSPricing:
         
         return retvalue
    
+    #######################################################################
+    # URL lookup for region SP version  Url
+    #######################################################################
+    def getSKUListLocal(self, pRegionCode):
+        myJSON = None
+        counter = None
+        instanceType = None
+        pattern = "^(.+)\.([0-9A-Za-z]+)"
+        with open('index_aws_ec2.json') as json_file:
+            myJSON = json.load(json_file)   # note: json.load() for local file instead of json.loads() 
+        #print(len(myJSON["products"]))
+        counter = 0
+        #print(myJSON["products"])
+        #print(myJSON["products"][0]["sku"])
+        for key,value in myJSON["products"].items(): #range(len(myJSON["products"])):
+            if (value["productFamily"] == "Compute Instance" and value["attributes"]["servicecode"] == "AmazonEC2"
+                and value["attributes"]["operatingSystem"] == "Linux" # or value["attributes"]["operatingSystem"] == "RHEL")
+                and value["attributes"]["preInstalledSw"] == "NA"
+                and value["attributes"]["instanceFamily"] == "General purpose"
+                and value["attributes"]["locationType"] == "AWS Region"
+                and value["attributes"]["tenancy"] == "Shared"
+                and value["attributes"]["location"] == self.getAWSLocationFromCode(pRegionCode)):
+
+                if ("instanceType" in value["attributes"] and re.match(pattern,value["attributes"]["instanceType"])):
+                    m = re.search(pattern, value["attributes"]["instanceType"])
+                    if (m.group(2) == "small"):
+                        print (key + ": " + m.group(0))
+                        counter = counter + 1            
+                
+            if (counter > 10):
+                break
+        #for x in range(len(myJSON["products"])):
 
 
 ############################################
@@ -110,5 +168,7 @@ if __name__ == '__main__':
     spURL = myObj.getOfferIndexURL()
     
     
-    myObj.getSavingsPlanPriceListForRegion('IAD', spURL)
+    #myObj.getSavingsPlanPriceListForRegion('IAD', spURL)
+
+    myObj.getSKUListLocal("LHR")
     #print(myObj.getAWSRegionFromCode('DUB'))
